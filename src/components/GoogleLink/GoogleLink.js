813768c6dcca95482c75a06121e62a53fad7fd49
Daniele Santos <danielesantos@Danieles-MacBook-Pro.local>
2017-11-17 00:25:34 +0100

    rounded button and transparent logo

--- src/components/GoogleLink/GoogleLink.js
+++ src/components/GoogleLink/GoogleLink.js
@@ -6,7 +6,7 @@ class GoogleLink extends React.Component{
 		return(
 			<div className="GoogleLink-container">
 				<div className='GoogleLink'>
-          <img className="GoogleLink-logo" src={require('./../../assets/logo.png')}/>
+          <img className="GoogleLink-logo" alt="Fling Logo" src={require('./../../assets/logo.png')}/>
 					<a className="GoogleLink-link" href={`${process.env.REACT_APP_API_URL}/auth/google_oauth2?auth_origin_url=${process.env.REACT_APP_ORIGIN_URL}`}>Sign in with Google</a>
 				</div>
 			</div>
