92b415f50598f526582d23c4cd56335e3fe4a6e8
alexmorten <alexandrius.martinus@gmx.de>
2017-11-17 10:27:41 +0100

    use right asset importing

--- src/components/GoogleLink/GoogleLink.js
+++ src/components/GoogleLink/GoogleLink.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './GoogleLink.css';
-
+import Logo from '../../assets/logo.png';
 class GoogleLink extends React.Component{
 	render(){
 		return(
 			<div className="GoogleLink-container">
 				<div className='GoogleLink'>
-          <img className="GoogleLink-logo" alt="Fling Logo" src={require('./../../assets/logo.png')}/>
+          <img className="GoogleLink-logo" alt="Fling Logo" src={Logo}/>
 					<a className="GoogleLink-link" href={`${process.env.REACT_APP_API_URL}/auth/google_oauth2?auth_origin_url=${process.env.REACT_APP_ORIGIN_URL}`}>Sign in with Google</a>
 				</div>
 			</div>
